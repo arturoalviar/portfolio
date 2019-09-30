0525d036f1f4b8c5b5c0c43b62d92db9a8252b61
arturoalviar <arturoalviar@gmail.com>
2019-09-30 19:53:50 -0400

    removed extra themeprovider

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,27 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ThemeProvider } from 'styled-components'
 import { SEO, Footer } from '@components'
-import theme from '@styles/theme.js'
 
 const Layout = ({ children, hasFooter }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <>
-        <SEO />
-        {children}
-        {hasFooter && <Footer />}
-      </>
-    </ThemeProvider>
+    <>
+      <SEO />
+      {children}
+      {hasFooter && <Footer />}
+    </>
   )
 }
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-  theme: PropTypes.object.isRequired,
   hasFooter: PropTypes.bool,
 }
 Layout.defaultProps = {
-  theme: theme,
   hasFooter: true,
 }
 
