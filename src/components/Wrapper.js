5cb2d52e0689ff240e10ae331fa8b8083c646e81
arturoalviar <arturoalviar@gmail.com>
2020-02-25 12:00:54 -0500

    moved children to ThemeContext component; use ThemeContext component

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,23 +1,10 @@
 import React from 'react'
-import GlobalStyle from '@styles/global.js'
-import theme from '@styles/theme.js'
-import { Header, Transition } from '@components'
-import { ThemeProvider } from 'styled-components'
+import { ThemeContext } from '@components'
 import 'typeface-muli'
 import 'typeface-playfair-display'
 
 const Wrapper = ({ children, location }) => {
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyle />
-          <Header />
-          <Transition location={location}>{children}</Transition>
-        </>
-      </ThemeProvider>
-    </>
-  )
+  return <ThemeContext location={location}>{children}</ThemeContext>
 }
 
 export default Wrapper
