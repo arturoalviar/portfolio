c5ed9d7f3b9f17075ce6dda47e2c5961747cbbe8
arturoalviar <arturoalviar@gmail.com>
2020-02-28 21:10:28 -0500

    created helper to resolve prismic links

---
+++ src/utils/linkResolver.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const linkResolver = function linkResolver(doc) {
+  return `/${doc.uid}`
+}
+
+const customLink = (type, element, content, children, index) => (
+  <Link key={element.data.id} to={linkResolver(element.data)}>
+    {content}
+  </Link>
+)
+
+export { customLink, linkResolver }
