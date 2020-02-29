ef95580bed33ec670f957fd215c80ec76b2d2541
arturoalviar <arturoalviar@gmail.com>
2020-02-28 21:49:24 -0500

    moved content to prismic; added graphql query

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
+import { graphql } from 'gatsby'
+import { RichText } from 'prismic-reactjs'
 import { Banner, Container, Layout, Section, SEO } from '@components'
 import { useThemeWrapper } from '@components/ThemeContext'
 
@@ -15,9 +17,7 @@ const UsesDiv = styled.div`
   }
 `
 
-const usesTagline = 'A list of the tools and gear I use on a daily basis.'
-
-const Uses = ({ data }) => {
+const Uses = ({ data: { prismic } }) => {
   const themeToggle = useThemeWrapper()
   // will move this logic into its own component if I need it for a different page
   useEffect(() => {
@@ -27,71 +27,42 @@ const Uses = ({ data }) => {
     }
   }, [themeToggle])
 
+  const { title, tagline, sections } = prismic.uses
+
   return (
     <Layout>
-      <SEO title="Uses" description={usesTagline} />
-      <Banner title="Every dev has their arsenal" variant="inverse">
-        <h4>{usesTagline}</h4>
+      <SEO title="Uses" description={tagline} />
+      <Banner title={title[0].text} variant="inverse">
+        <h4>{tagline}</h4>
       </Banner>
       <Section variant="inverse">
         <Container>
-          <UsesDiv>
-            <h2>Editor + Terminal</h2>
-            <ul>
-              <li>
-                Atom - I have been using Atom since my junior year of college
-                (2013). I am also biased since I've made a number of themes for
-                Atom.
-              </li>
-              <li>
-                Visual Studio Code - If for some reason I can't use Atom, Code
-                is a great alternative. One of the advantages it has over Atom
-                is that it does not crash when opening a big file.
-              </li>
-              <li>
-                Hyper - I use to only use iTerm 2 but the ease of customization
-                makes Hyper my goto terminal app.
-              </li>
-            </ul>
-          </UsesDiv>
-          <UsesDiv>
-            <h2>Desktop Apps</h2>
-            <ul>
-              <li>
-                f.lux - MacOS now has the functionality of f.lux built in but I
-                prefer the app since it is slightly more customizable.
-              </li>
-              <li>
-                Sketch - I have been using Sketch since 2015 and it is my goto
-                whenever I want to design something.
-              </li>
-              <li>
-                Photoshop - I learned how to use Photoshop in high school in
-                order to work on some projects for AP Art and since have become
-                fairly comfortable with it. Now, I mainly use it to enhance
-                photos.
-              </li>
-            </ul>
-          </UsesDiv>
-          <UsesDiv>
-            <h2>Dev Battlestation</h2>
-            <ul>
-              <li>Laptop - 2017 15 inch Macbook Pro</li>
-              <li>Monitor - LG 32 4k UD99W</li>
-              <li>Speakers - Kanto YU 5</li>
-              <li>
-                Keyboard - RAMA M-60a, Tokyo60, or RGBKB Sol 2 (depends on the
-                day)
-              </li>
-              <li>Mouse - Logitech MX Master v1</li>
-              <li>Chair - Steelcase Series 1</li>
-              <li>Desk - IKEA Kitchen counter top</li>
-            </ul>
-          </UsesDiv>
+          {sections.map((section, index) => {
+            const { content } = section
+            return (
+              <UsesDiv key={`uses-div-${index}`}>
+                <RichText render={content} />
+              </UsesDiv>
+            )
+          })}
         </Container>
       </Section>
     </Layout>
   )
 }
+
+export const usesQuery = graphql`
+  query UsesQuery {
+    prismic {
+      uses(lang: "en-us", uid: "uses") {
+        title
+        tagline
+        sections {
+          content
+        }
+      }
+    }
+  }
+`
 
 export default Uses
